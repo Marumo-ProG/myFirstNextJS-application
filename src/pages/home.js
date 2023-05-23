7eb09e8e5c3b2c503e44a5d8cf5a50cfe4eb8fa8
Marumo-ProG <Lennythobejane@gmail.com>
2023-05-23 13:01:58 +0200

    finished the bottom navigation pane buttons

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,16 @@
 import "@fontsource/roboto";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login"
+import {useState} from "react";
 import { Typography, Grid, Container, Link, Button } from "@mui/material";
 const Home = () => {
+  const [show,setShow] = useState("signup");
+  const handleShowLogin = () =>{
+    setShow("login");
+  }
+  const handleShowSignup = () =>{
+    setShow("signup");
+  }
   return (
     <div>
       <h2 style={{ textAlign: "center", fontFamily: "roboto" }}>
@@ -10,11 +18,10 @@ const Home = () => {
       </h2>
       <hr style={{width: "50%", marginLeft: "auto", marginRight: "auto"}}/>
       <Container style={{width: "70%"}}>
-        <Login />
-
-        <div className="bottom-nav">
-          <Button>SignUp</Button>
-          <Button>login</Button>
+        {show=="signup" ? <SignUp /> : <Login />}
+        <div className="bottom-nav" style={{position: "absolute"}}>
+          {show=="login" ? <Button onClick={handleShowSignup}>SignUp</Button> : <Button style={{borderBottom: "2px solid blue"}} onClick={handleShowSignup}>SignUp</Button> }
+          {show=="signup" ? <Button onClick={handleShowLogin}>login</Button> : <Button style={{borderBottom: "2px solid blue"}} onClick={handleShowLogin}>login</Button> }
         </div>
       </Container>
     </div>
